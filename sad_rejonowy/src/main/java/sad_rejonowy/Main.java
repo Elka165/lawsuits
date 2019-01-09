187b47c175f91df92b12537dd3599ca22edec609
Elka165 <ela_dolna99@o2.pl>
2019-01-09 14:17:16 +0100

    sprawy sadowe SWING -shema

---
+++ sad_rejonowy/src/main/java/sad_rejonowy/Main.java
@@ -0,0 +1,12 @@
+package sad_rejonowy;
+
+import sad_rejonowy.gui.Login;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        JudicialFeesPanel judicialFeesPanel =new JudicialFeesPanel();
+//        judicialFeesPanel.mainFrame.setVisible(true);
+       Login login=new Login();
+    }
+}
